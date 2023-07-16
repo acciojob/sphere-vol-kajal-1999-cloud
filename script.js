2db67cba3cb9640607b8fd20ea8ae197e1196592
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-16 20:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-function volume_sphere(event) {
+function volume_sphere() {
     //Write your code here
-	event.preventDefault();
-	const radius = document.getElementById("radius").value;
-	radius = math.abs(radius);
-	const volume = (4/3) * Math.PI * Math.pow(radius, 3);
-	volume = volume.toFixed(4);
+	// event.preventDefault();
+	var radius = document.getElementById("radius").value;
+	radius = Math.abs(radius);
+	var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);//keep the value till 4 decimal places
 	document.getElementById('volume').value = volume ;
-  // return false; // to prevent the default behaviour of browser so that 
+   return false; // to prevent the default behaviour of browser so that 
 	// value will be shown in the 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+
+
+ 
