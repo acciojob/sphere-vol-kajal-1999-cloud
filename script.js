93abfcc6c3da320a9a427a8e17a95d04d729a791
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-16 20:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	const radius = document.getElementById("radius").value;
+	radius = math.abs(radius);
+	const volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	volume = volume.toFixed(4);
+	document.getElementById('volume').value = volume ;
+  // return false; // to prevent the default behaviour of browser so that 
+	// value will be shown in the 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
